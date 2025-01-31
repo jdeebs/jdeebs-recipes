2463a0ec9aa1b6928d0822e8c3da95ebfd39583b
Jacob DiBiase <jakesback93@hotmail.com>
2025-01-31 11:49:36 -0700

    Update allowed hosts with site and development url

--- recipe_project/settings.py
+++ recipe_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY','django-insecure-ml49cp(e)=yakpe
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['secret-reef-56392-9a358239c36e.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
