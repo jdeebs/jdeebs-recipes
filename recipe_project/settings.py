94e01ad69bc8d57d2118a5c5b45e88a7c7030b0d
Jacob DiBiase <jakesback93@hotmail.com>
2025-01-31 12:13:56 -0700

    Fix database config for Heroku

--- recipe_project/settings.py
+++ recipe_project/settings.py
@@ -81,8 +81,7 @@ WSGI_APPLICATION = 'recipe_project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'default': dj_database_url.config(default=f'sqlite:///{BASE_DIR / "db.sqlite3"}', conn_max_age=500)
     }
 }
 
@@ -137,8 +136,3 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # Auth
 LOGIN_URL='/login/'
-
-# Heroku: Update database configuration from $DATABASE_URL.
-import dj_database_url
-db_from_env = dj_database_url.config(conn_max_age=500)
-DATABASES['default'].update(db_from_env)
