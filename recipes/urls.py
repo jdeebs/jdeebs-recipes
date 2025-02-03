933ed9e1a716f27918aae1d14cdde3c3b1523faa
Jacob DiBiase <jakesback93@hotmail.com>
2025-02-03 13:43:54 -0700

    Update urls.py with add recipe path

--- recipes/urls.py
+++ recipes/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import home, RecipeListView, RecipeDetailView, delete_recipe
+from .views import home, RecipeListView, RecipeDetailView, delete_recipe, add_recipe
 
 app_name = 'recipes'
 
@@ -8,4 +8,5 @@ urlpatterns = [
     path('recipes/', RecipeListView.as_view(), name='list'),
     path('recipes/<pk>', RecipeDetailView.as_view(), name='detail'),
     path('recipes/delete/<int:pk>', delete_recipe, name='delete_recipe'),
+    path('recipes/add/', add_recipe, name='add_recipe')
 ]
