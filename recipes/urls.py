bed8e60cf73ad1614d6cf33d6d6e0cea210e9ccf
Jacob DiBiase <jakesback93@hotmail.com>
2025-02-03 11:55:38 -0700

    Include delete_recipe route in urls.py

--- recipes/urls.py
+++ recipes/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import home, RecipeListView, RecipeDetailView
+from .views import home, RecipeListView, RecipeDetailView, delete_recipe
 
 app_name = 'recipes'
 
@@ -7,4 +7,5 @@ urlpatterns = [
     path('', home, name='home'),
     path('recipes/', RecipeListView.as_view(), name='list'),
     path('recipes/<pk>', RecipeDetailView.as_view(), name='detail'),
+    path('recipe/delete/<int:pk', delete_recipe, name='delete_recipe'),
 ]
